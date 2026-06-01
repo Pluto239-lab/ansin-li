81d16bd4008a07f757d4700dec05e4029951cd8c
Pluto239-lab <ah1165910016@gmail.com>
2026-06-01 12:30:29 +0900

    Fix toggleAll function assignment to checkAll

--- api.js
+++ api.js
@@ -1,8 +1,6 @@
 const checkAll = document.getElementById("checkAll");
-
 function toggleAll() {
     const checkBoxes = document.querySelectorAll('.detail input[type="checkbox"]');
     checkBoxes.forEach(cb => cb.checked = checkAll.checked);
 }
-
 checkAll.onclick = toggleAll;
